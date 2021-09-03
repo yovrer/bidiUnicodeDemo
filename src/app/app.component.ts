0bf2d2d10101de5debeb538ad37454fa5007fbf2
Ahmad Nabas <ahmad.nabas1994@gmail.com>
2021-09-03 19:43:58 +0300

    added raido button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {textArr} from "./text-data";
 import * as _ from "lodash";
 import {Direction} from "@angular/cdk/bidi/directionality";
+import * as he from "he";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   texts = textArr;
   selectedText = this.texts[7];
-  inputValue = _.cloneDeep(this.selectedText);
+  inputValue = '';
+  preview = '';
+
   dir: Direction = 'ltr';
-  directions = ['ltr','rtl','auto']
+  directions = ['ltr', 'rtl', 'auto']
+  he = he;
 
   onChange(value: string) {
-    this.inputValue = _.cloneDeep(value);
+    // this.inputValue = _.cloneDeep(value);
+    console.log('value', value)
+    console.log('he.decode(value)', he.decode(value))
+    this.preview = _.cloneDeep(he.decode(value));
+
+  }
+
+  ngOnInit(): void {
+    this.updateInputValue();
   }
 
   updateInputValue() {
     this.inputValue = _.cloneDeep(this.selectedText);
+    this.preview = _.cloneDeep(this.inputValue);
   }
 }
