139e05e068e9d7b47ca5678422226bff22cdfa91
Ahmad Nabas <ahmad.nabas1994@gmail.com>
2021-09-04 18:45:38 +0300

    added radio button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import * as _ from "lodash";
 import {Direction} from "@angular/cdk/bidi/directionality";
 import * as he from "he";
 
+export interface Unicode {
+  abbreviation: string;
+  code: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +23,16 @@ export class AppComponent implements OnInit {
   preview = '';
 
   dir: Direction = 'ltr';
+  unicodeArr: Unicode[] = [
+    {abbreviation: 'LRE', code: 'U+200A', description: 'Left to Right Embedding'},
+    {abbreviation: 'RLE', code: 'U+200B', description: 'Right to Left Embedding'},
+    {abbreviation: 'PDF', code: 'U+200C', description: 'Pop Directional Formatting (ending an embedding)'},
+    {abbreviation: 'LRI', code: 'U+2066', description: 'Left to Right Isolate'},
+    {abbreviation: 'RLI', code: 'U+2067', description: 'Right to Left Isolate'},
+    {abbreviation: 'FSI', code: 'U+2068', description: 'First Strong Isolate'},
+    {abbreviation: 'PDI', code: 'U+2069', description: 'Pop Directional Isolate (ending an isolate)'},
+  ];
+  selectionStart: number = 0;
   directions = ['ltr', 'rtl', 'auto']
   he = he;
 
@@ -31,6 +47,19 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.updateInputValue();
   }
+
+  onTextAreaClick(textArea: HTMLTextAreaElement) {
+    this.selectionStart = textArea.selectionStart;
+  }
+
+  onUnicodeButtonClick(unicode: Unicode) {
+    this.inputValue = this.insertAt(this.selectionStart, this.inputValue, unicode.code);
+    console.log(this.inputValue);
+  }
+
+  insertAt(selectionStart: number, inputValue: string, code: string): string {
+    return inputValue.slice(0, selectionStart) + code + inputValue.slice(selectionStart + Math.abs(0));
+  };
 
   updateInputValue() {
     this.inputValue = _.cloneDeep(he.decode(this.selectedText));
